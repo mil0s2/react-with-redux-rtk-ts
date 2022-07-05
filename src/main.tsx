936cee13f50062a416fcb70835756b7865c4a4e2
mil0s2 <miloszmazurk@gmail.com>
2022-07-05 16:04:31 +0200

    setting types

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
