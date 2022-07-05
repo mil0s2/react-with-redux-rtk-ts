7bb59243b2650b01740d7c7fadd31caa060ad61a
mil0s2 <miloszmazurk@gmail.com>
2022-07-05 15:42:52 +0200

    first commit

---
+++ src/components/ExpensesHistory.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpensesHistory = () => {
+  return <div>ExpenseHistory</div>;
+};
+
+export default ExpensesHistory;
---
+++ src/components/IncomeAndExpenses.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IncomeAndExpenses = () => {
+  return <div>IncomeAndExpenses</div>;
+};
+
+export default IncomeAndExpenses;
