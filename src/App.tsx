a852da186998233404efe4666ae086f777946cd9
mil0s2 <miloszmazurk@gmail.com>
2022-07-11 20:02:32 +0200

    styling components with functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import Balance from "./components/Balance";
+import ExpensesHistory from "./components/ExpensesHistory";
+import TransactionForm from "./components/TransactionForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="w-full items-center p-[20%] flex flex-col gap-4">
+      <Balance />
+      <TransactionForm />
+      <ExpensesHistory />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Stack } from "react-bootstrap";
+import { useAppSelector } from "../hooks/hooks";
 
 const Balance = () => {
-  return <div>Balance</div>;
+  const balance = useAppSelector((state) => state.expense.balance);
+  const income = useAppSelector((state) => state.expense.income);
+  const expense = useAppSelector((state) => state.expense.expense);
+  return (
+    <div className="px-4 py-4 mt-4 flex justify-between w-full bg-gray-100 rounded-md font-semibold">
+      <Stack className="flex flex-col items-center w-full gap-2">
+        <div className="text-lg">Your balance: ${balance}</div>
+        <div className="flex justify-center bg-gray-50 w-full py-2 gap-8 rounded-md">
+          <div className="flex flex-col">
+            Income: <span className="text-green-600">${income}</span>
+          </div>
+          <div className="flex flex-col">
+            Expense: <span className="text-red-600">${expense}</span>
+          </div>
+        </div>
+      </Stack>
+    </div>
+  );
 };
 
 export default Balance;
